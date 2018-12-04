625176ee5daee550e0c78f57ea0d83b51d88b654
Nicolas "0swin" Roger <nicolas@oswin.io>
2018-12-04 18:22:23 +0100

    Random Rotation + Stroke Color

--- js/flatland.js
+++ js/flatland.js
@@ -68,7 +68,7 @@ function createGlobalColor() {
 
 var globalHue = createGlobalColor()
 
-function createFacePart(height, width, posMin, posMax, calque, calqueMin, calqueMax) {
+function createFacePart(height, width, posMin, posMax, rotMin, rotMax, calque, calqueMin, calqueMax) {
   // GÉNÉRER UNE DIV POUR CHAQUE MORCEAU DE VISAGE
   var facePart = document.createElement("div");
   facePart.id = "facePart" + i;
@@ -77,16 +77,17 @@ function createFacePart(height, width, posMin, posMax, calque, calqueMin, calque
   facePart.style.position = "absolute";
   facePart.style.height = height + "px";
   facePart.style.width = width + "px";
-
+  //ROTATION ALEATOIRE
+  facePart.style.transform = "rotate(" + getRandomInt(rotMin, rotMax) + "deg)";
   // POSITION ALEATOIRE
   facePart.style.top = getRandomInt(posMin, posMax) + "px";
   facePart.style.left = getRandomInt(posMin, posMax) + "px";
-
-  // COULEUR ALEATOIRE
+  // COULEUR FOND ALEATOIRE
   var hue = globalHue + getRandomInt(-30, 30)
-  var saturation = 80 + getRandomInt(-30, 30)
-  var lightness = 50 + getRandomInt(-30, 30)
+  var saturation = getRandomInt(60, 100)
+  var lightness = getRandomInt(40, 60)
   var hsl = "hsl(" + hue + "," + saturation + "%," + lightness + "%)";
+  var hslStroke = "hsl(" + hue + "," + (saturation + 20) + "%," + (lightness - 30) + "%)";
   console.log(hsl);
 
   var animationData = {
@@ -102,19 +103,23 @@ function createFacePart(height, width, posMin, posMax, calque, calqueMin, calque
   //we add an event to get all the elements when tey are add to the DOM
   anim.addEventListener('DOMLoaded', function() {
     // console.log("facePart" + index);
-    let child = getAllChild("facePart" + index, ".fill");
-    for (let j = 0; j < child.length; j++) {
+    let childFill = getAllChild("facePart" + index, ".fill");
+    for (let j = 0; j < childFill.length; j++) {
       // console.log(child[j]);
-      child[j].style.fill = hsl;
+      childFill[j].style.fill = hsl;
+    };
+    let childStroke = getAllChild("facePart" + index, ".stroke");
+    for (let j = 0; j < childStroke.length; j++) {
+      childStroke[j].style.stroke = hslStroke;
     };
     facePart.style.opacity = 1;
   });
 };
 
 for (var i = 1; i <= 3; i++) {
-  createFacePart(350, 350, 0, 25, "calque2", 1, 7);
+  createFacePart(350, 350, 0, 25, 0, 360, "calque2", 1, 8);
 };
 
 for (var i = 4; i <= 7; i++) {
-  createFacePart(100, 100, 25, 225, "calque1", 1, 7);
+  createFacePart(100, 100, 25, 225, -30, 0, "calque1", 1, 7);
 };
